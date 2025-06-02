0384773a46f501b3f2d4b5f8c7761651b2046a4f
MediCat720 <maidmedi720@gmail.com>
2025-06-01 21:58:00 -0500

    add a FAQ page

---
+++ app/faq/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+return (
+    <div className="max-w-xl mx-auto p-8">
+        <h1 className="text-3xl font-bold mb-6 text-center">Frequently Asked Questions</h1>
+        <p className="mb-4 text-white-700">
+            What is Chat Validation Error?/Why can't I see skins from certain players?
+        </p>
+        <p className="mb-4 text-white-700">
+            What is the server version? 1.21.5
+        </p>
+        <p className="mb-4 text-white-700">
+            Why can't I join the Minecraft server?/What is whitelist?
+        </p>
+        <p className="mb-4 text-white-700">
+            See join page (add hyperlink)
+        </p>
+        
+    </div>
+)
+}
--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,16 +1,14 @@
 export default function Page() {
 return (
     <div className="max-w-xl mx-auto p-8">
-        <h1 className="text-3xl font-bold mb-6 text-center">Welcome to Next.js!</h1>
+        <h1 className="text-3xl font-bold mb-6 text-center">How To Join Duneraft</h1>
         <p className="mb-4 text-white-700">
-            This is a sample page demonstrating how to use Tailwind CSS with Next.js. Tailwind makes it easy to style your components with utility classes.
+            info: https://dualie.ink/drasl/authlib-injector
         </p>
         <p className="mb-4 text-white-700">
-            You can quickly build modern, responsive layouts by combining these classes directly in your JSX.
+            version: 1.21.5
         </p>
-        <p className="text-white-700">
-            Explore the documentation to learn more about Next.js and Tailwind CSS integration.
-        </p>
+        
     </div>
 )
 }
